797c15c4c7e851e4e02270a4f2602e68fcee2ad9
Danae Panagiotopoulou <danai231@windowslive.com>
2023-11-26 07:57:49 +0100

    part 3

--- week1/wks1.3.js
+++ week1/wks1.3.js
@@ -14,14 +14,14 @@ window.onload= function init(){
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
    
-
     var vertices = [ vec2(0.0, 0.0), vec2(1.0, 1.0), vec2(1.0, 0.0) ];
     var colors = [
         vec3(1.0, 0.0, 0.0),
         vec3(0.0, 0.0, 1.0),
         vec3(0.0, 1.0, 0.0)
         ];
-        var colorsArray = [ ];
+
+    var colorsArray = [ ];
     for (var index = 0; index < 3; ++index) {
     //determine which color[i] to assign to pointsArray[index]
     colorsArray.push(colors[index]);
@@ -31,7 +31,7 @@ window.onload= function init(){
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
     
-  
+    //color buffer
     var cBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW );
@@ -47,7 +47,5 @@ window.onload= function init(){
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition)
     
-       
- 
     gl.drawArrays(gl.TRIANGLES,0,3)
 }
